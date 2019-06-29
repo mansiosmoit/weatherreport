0cfaa5fec1e21bd0e0d1ec71e82d5cfe494647b8
root <root@localhost.localdomain>
2019-06-29 19:10:55 +0530

    9 commit

--- WeatherForecast/app/src/main/java/com/example/weatherforecast/Tomorrow.java
+++
@@ -1,6 +0,0 @@
-package com.example.weatherforecast;
-
-import android.support.v4.app.Fragment;
-
-public class Tomorrow extends Fragment {
-}
--- WeatherForecast/app/src/main/java/com/example/weatherforecast/fiveDays.java
+++
@@ -1,6 +0,0 @@
-package com.example.weatherforecast;
-
-import android.support.v4.app.Fragment;
-
-public class fiveDays extends Fragment {
-}
